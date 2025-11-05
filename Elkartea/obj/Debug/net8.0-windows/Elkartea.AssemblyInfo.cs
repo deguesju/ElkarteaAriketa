54aefab8bf0edf3f38217b07bf3de011b476bff0
deguesju <deguesju25paag@ikzubirimanteo.com>
2025-11-05 08:57:37 +0100

    Euskerara itzulita

--- Elkartea/obj/Debug/net8.0-windows/Elkartea.AssemblyInfo.cs
+++ Elkartea/obj/Debug/net8.0-windows/Elkartea.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elkartea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5315ce514d5758a8f7a613f888db0b375c0610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8249095b73bd712c16b88d50dcd9aa58986eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elkartea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elkartea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
